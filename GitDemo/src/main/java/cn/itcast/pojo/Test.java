a981bb9d0f5101ef598f8eb5c943d8fd18acf9fc
fenyanlong <13716134783@163.com>
2018-10-28 21:22:12 +0800

    测试

---
+++ GitDemo/src/main/java/cn/itcast/pojo/Test.java
@@ -0,0 +1,12 @@
+package cn.itcast.pojo;
+
+/**
+ * @ClassName:Test
+ * @Description: TODO
+ * @Date:2018/10/2821:21
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("你好啊");
+    }
+}

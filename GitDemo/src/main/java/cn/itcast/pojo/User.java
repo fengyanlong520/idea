ef097ba6ea42f3adc41242d367ca52f6661d4745
fenyanlong <13716134783@163.com>
2018-10-28 21:11:16 +0800

    初始化导入工程

---
+++ GitDemo/src/main/java/cn/itcast/pojo/User.java
@@ -0,0 +1,17 @@
+package cn.itcast.pojo;
+
+/**
+ * @ClassName:User
+ * @Description:
+ * @Date:2018/10/2818:21
+ **/
+public class User {
+    public static void main(String[] args) {
+        System.out.println("欢迎来带英雄联盟");
+
+        System.out.println("学习");
+    }
+
+    private String name;
+
+}
